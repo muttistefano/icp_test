a238f0c5734bfccf81aaf3cbb745b5cd8904c327
Stefano <stefano.mutti@stiima.cnr.it>
2022-06-28 15:46:30 +0200

    S

--- scripts/transfer/prepare_data_fine.py
+++ scripts/transfer/prepare_data_fine.py
@@ -176,11 +176,14 @@ print(tf_tot.nbytes * 1e-6)
 
 # print("lasers mean and std: " + str(laser_tot.mean()) + " " + str(laser_tot.std()))
 
-tf_min_max = np.load("tf_min_max.npy")
+# tf_min_max = np.load("tf_min_max.npy")
 
-tf_tot[:,0] = (tf_tot[:,0] - tf_min_max[0]) / (tf_min_max[1] - tf_min_max[0])
-tf_tot[:,1] = (tf_tot[:,1] - tf_min_max[2]) / (tf_min_max[3] - tf_min_max[2])
-tf_tot[:,2] = (tf_tot[:,2] - tf_min_max[4]) / (tf_min_max[5] - tf_min_max[4])
+# tf_tot[:,0] = (tf_tot[:,0] - tf_min_max[0]) / (tf_min_max[1] - tf_min_max[0])
+# tf_tot[:,1] = (tf_tot[:,1] - tf_min_max[2]) / (tf_min_max[3] - tf_min_max[2])
+# tf_tot[:,2] = (tf_tot[:,2] - tf_min_max[4]) / (tf_min_max[5] - tf_min_max[4])
+tf_tot[:,0] = (tf_tot[:,0] - tf_tot[:,0].min()) / (tf_tot[:,0].max() - tf_tot[:,0].min())
+tf_tot[:,1] = (tf_tot[:,1] - tf_tot[:,1].min()) / (tf_tot[:,1].max() - tf_tot[:,1].min())
+tf_tot[:,2] = (tf_tot[:,2] - tf_tot[:,2].min()) / (tf_tot[:,2].max() - tf_tot[:,2].min())
 
 # print("laser max : " + str(laser_tot.max()))
 # laser_tot /= laser_tot.max()
